915de756f06ff8a8155e864fdf092df8c592466f
robert-gleis <robert.gleis@gmx.de>
2025-09-13 20:14:43 +0200

    Update site configuration and add CNAME for new domain
    - Changed site URL in astro.config.mjs to 'https://www.eldevel.de' for the new domain.
    - Updated image domains to reflect the new site URL.
    - Added a CNAME file in the public directory to support custom domain configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,14 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://robert-gleis.github.io',
-	base: process.env.NODE_ENV === 'production' ? '/eldevel-page' : '/',
+	site: 'https://www.eldevel.de',
+	base: '/',
 	output: 'static',
 	integrations: [
 		sitemap(),
 		tailwind()
 	],
 	image: {
-		domains: ['robert-gleis.github.io'],
+		domains: ['www.eldevel.de'],
 	},
 });
